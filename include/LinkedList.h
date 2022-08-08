af344a8fb72c53f3ae554ddd2be276b407afe6c7
Sangam Ghimire <1sangamghimire1@gmail.com>
2022-08-08 15:17:39 +0545

    Created a include folder, src folder and main.cpp the plan is to do it this way

---
+++ include/LinkedList.h
@@ -0,0 +1,33 @@
+#ifndef LinkedList_h
+#define LinkedList_h
+
+class Node{
+public:
+	int info;	//to store data
+	Node* next;	//to point to next node
+
+	Node(){} //default constructor
+	~Node(){} //default deconstructor
+	Node(int d):info(d), next(nullptr){};
+	Node(int d, Node* next): info(d), next(next){};
+};
+
+class LinkedList{
+
+private:
+	Node* Head;
+	Node* Tail;
+public:
+	bool isEmpty();
+
+	void addToHead(Node *nodetobeadded);
+	void addToTail(Node *nodetobeadded);
+	void add(Node *nodetobeadded);
+
+	void removeFromHead();
+	void removeFromTail();
+	void remove(int data);
+	Node* retrieve(int data);
+	bool search(int datatobesearched);
+	void traverse();
+};
